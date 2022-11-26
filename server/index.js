24d44e4d8db4c541e969feecb1f6f20a65e03d6a
vaishnavihole <vaishnavihole1@gmail.com>
2022-11-26 10:54:07 +0530

    feat: receivable payables route added successfully

--- server/index.js
+++ server/index.js
@@ -7,35 +7,57 @@ dotenv.config();
 
 
 const app = express();
-app.use(express.json()) 
+app.use(express.json())
 
-app.post('/transactionItem', async(req, res) =>{
-    const {title, amount, itemType, note} = req.body
-   
+app.post('/transactionItem', async (req, res) => {
+    const { title, amount, itemType, note } = req.body
+
     const newTransactionItem = new TransactionItem({
         title: title,
         amount: amount,
         itemType: itemType,
         note: note
     })
-   
+
     const savedItem = await newTransactionItem.save()
-   
+
     res.send({
         message: 'data saved successfully',
-        data:  savedItem
+        data: savedItem
     })
 })
 
-app.get('/transactionItem', async (req, res)=>{
+app.get('/transactionItem', async (req, res) => {
     const transactionItems = await TransactionItem.find()
-
     res.send({
         message: 'data fetched successfully',
         data: transactionItems
     })
 })
 
+app.get('/receivables', async (req, res) => {
+    const receivables = await TransactionItem.find({
+        itemType: 'receivable'
+    })
+    
+    res.send({
+        message: 'receivable item fetched successfully',
+        data: receivables
+    })
+})
+
+app.get('/payables', async (req, res) =>{
+    const payables = await TransactionItem.find({
+        itemType: 'payable'
+    })
+
+    res.send({
+        message: 'payables item  fetched successfully',
+        data: payables
+    })
+})
+
+    
 mongoose.connect(process.env.MONGO_DB_URL, () => {
     console.log("Connected to mongo DB📦")
 })
