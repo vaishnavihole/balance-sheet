bd3c8f895c1ff49e1b1a65166e040c8365d5ef4b
vaishnavihole <vaishnavihole1@gmail.com>
2022-12-03 11:22:18 +0530

    css design for add transaction view

--- client/src/views/AddTransaction/AddTransaction.js
+++ client/src/views/AddTransaction/AddTransaction.js
@@ -1,22 +1,22 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios'
 import './AddTransaction.css'
 
 function AddTransaction() {
     const [title, setTitle] = useState("")
     const [amount, setAmount] = useState("")
-    const [itemType, setItemType] =useState("")
+    const [itemType, setItemType] = useState("")
     const [note, setNote] = useState("")
     const [category, setCategory] = useState("")
-    
-   function addTransactionItem() {
-      const response = axios.post("/transactionItem",{
-        title: title,
-        amount: amount,
-        itemType: itemType,
-        note: note,
-        category: category
-      })
+
+    function addTransactionItem() {
+        const response = axios.post("/transactionItem", {
+            title: title,
+            amount: amount,
+            itemType: itemType,
+            note: note,
+            category: category
+        })
         console.log(response.data)
         setTitle("")
         setAmount("")
@@ -28,14 +28,47 @@ function AddTransaction() {
     }
     return (
         <div>
-            <form>
-                <div><input type='text' placeholder='Enter Title' value={title} onChange={(e)=>{setTitle(e.target.value)}} required /></div>
-                <div><input type='text' placeholder='Enter Amount' value={amount} onChange={(e)=>{setAmount(e.target.value)}} required /></div>
-                <div><input type='text' placeholder='Enter ItemType' value={itemType}  onChange={(e) =>{setItemType(e.target.value)}} required /></div>
-                <div><input type='text' placeholder='Note' value={note} onChange={(e) =>{setNote(e.target.value)}} required /></div>
-                <div><input type='text' placeholder='Category' value={category} onChange={(e) =>{setCategory(e.target.value)}} required /></div>
-                <button type="button" onClick={addTransactionItem}>Add Transaction</button>
-            </form>
+            <div className='heading-container'>
+                <h1 className='heading'>💰 Add Transaction</h1>
+            </div>
+            <div className='form-container'>
+                <form>
+                    <div>
+                        <input className='form-input'
+                            type='text' placeholder='Enter Title' value={title}
+                            onChange={(e) => { setTitle(e.target.value) }} required />
+                    </div>
+
+                    <div>
+                        <input className='form-input'
+                            type='text' placeholder='Enter Amount' value={amount}
+                            onChange={(e) => { setAmount(e.target.value) }} required />
+                    </div>
+
+                    <div>
+                        <input className='form-input'
+                            type='text' placeholder='Enter ItemType' value={itemType}
+                            onChange={(e) => { setItemType(e.target.value) }} required />
+                    </div>
+
+                    <div>
+                        <input className='form-input'
+                            type='text' placeholder='Note' value={note}
+                            onChange={(e) => { setNote(e.target.value) }} required />
+                    </div>
+
+                    <div>
+                        <input className='form-input'
+                            type='text' placeholder='Category' value={category}
+                            onChange={(e) => { setCategory(e.target.value) }} required />
+                    </div>
+
+                    <div className='btn-add-transaction-container'>
+                        <button className='btn-add-transaction'
+                        type="button" onClick={addTransactionItem}>Add Transaction</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
