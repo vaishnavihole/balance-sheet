b50ae8142e755911fe3bac82216049c84a113808
vaishnavihole <vaishnavihole1@gmail.com>
2022-12-13 09:09:32 +0530

    added image

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import ImgHeader from './balance-sheet-header-img.jpeg'
 
 function Home() {
   return (
     <div>
       <Navbar />
-      Home</div>
+      <div className='row'>
+        <div className='col-md-6'>
+          <img className='header-image' src={ImgHeader} />
+        </div>
+        <div className='col-md-6'></div>
+        
+      </div>
+      </div>
   )
 }
 
